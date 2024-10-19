dd5af8eddcdc3fc4846b4a66c0df54d2e0d4c191
Pranjal Shinde <shindept2003@gmail.com>
2024-10-19 21:50:36 +0530

    Fixed Some errors

--- personal_portfolio/src/firebase.js
+++ personal_portfolio/src/firebase.js
@@ -4,23 +4,17 @@ import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNfFfDTJqHgVHbjyGE77gQzwlcwcALNWE",
-    authDomain: "pranjal-personal-portfolio.firebaseapp.com",
-    projectId: "pranjal-personal-portfolio",
-    storageBucket: "pranjal-personal-portfolio.appspot.com",
-    messagingSenderId: "96781308117",
-    appId: "1:96781308117:web:fb5b3dcd5d6d3451c8925c"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
 };
-
-// Initialize Firebase
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
